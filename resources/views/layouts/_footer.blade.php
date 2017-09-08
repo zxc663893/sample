2a6901991df8de78b382a700818aae9403922cdb
jiafeng <283678339@qq.com>
2017-09-08 01:28:17 +0000

    Finish layout and routes

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,15 @@
+<div class="col-md-12">
+  <footer class="footer">
+    <samll class="slogon">
+      <img class="brand-icon" src="https://dn-phphub.qbox.me/uploads/images/201612/12/1/iq7WQc2iuW.png?imageView2/1/w/34/h/34">
+      <a href="https://yousails.com">
+        在学PHP|刚刚开始,希望有个好结果
+      </a>
+    </samll>
+    <nav>
+      <ul>
+        <li><a href="{{ route('about') }}">关于</a></li>
+      </ul>
+    </nav>
+  </footer>
+</div>
---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,13 @@
+<header class="navbar navbar-fixed-top navbar-inverse">
+  <div class="col-md-offset-1 col-md-10">
+    <div>
+      <a herf="{{route('home')}}" id="logo">Sample App</a>
+      <nav>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a herf="{{ route('help') }}">帮助</a></li>
+          <li><a herf="#">登录</a></li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+</header>
