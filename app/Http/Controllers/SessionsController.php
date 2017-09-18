b0583f77cd4b8b1d41b26a4674b185a08c29215b
jiafeng <283678339@qq.com>
2017-09-18 08:54:28 +0000

    Add password resets & email configuration

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -31,7 +31,7 @@ class SessionsController extends Controller
           return redirect()->intended(route('users.show', [Auth::user()]));
       }else{
           Auth::logout();
-          session()->flash('warning', '你的帐号未激活，请检查邮箱中的注册邮件进行激活。 ')；
+          session()->flash('warning', '你的帐号未激活，请检查邮箱中的注册邮件进行激活。 ');
           return redirect('/');
         }
       }else {
